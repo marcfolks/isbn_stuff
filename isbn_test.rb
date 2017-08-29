108402137ed721531c0e68312b8d13f069d37543
marcfolks <marcfolks@gmail.com>
2017-08-29 15:45:43 -0400

    13thnumber

--- isbn_test.rb
+++ isbn_test.rb
@@ -4,14 +4,22 @@ class Testisbn < Minitest::Test
 
     def test_isbn_number_digits
         isbn_num="0471958697"
-        assert_equal(true,isbn_length(isbn_num))
+        assert_equal(true,isbn_length10(isbn_num))
     end
     def test_isbn_wrong_number_digits
         isbn_num="047195897"
-        assert_equal(false,isbn_length(isbn_num))
+        assert_equal(false,isbn_length10(isbn_num))
     end
     def test_isbn_dashes
         isbn_num="047-195-86-97"
         assert_equal("0471958697",isbn_remove(isbn_num))
+    end
+    def test_isbn_space
+        isbn_num="047 195 86 97"
+        assert_equal("0471958697",isbn_remove(isbn_num))
+    end
+    def test_isbn_13
+        isbn_num="0471958697123"
+        assert_equal(true,isbn_length13(isbn_num))
     end
 end
--- isbnfunction.rb
+++ isbnfunction.rb
@@ -1,6 +1,9 @@
-def isbn_length(isbn_num)
+def isbn_length10(isbn_num)
     isbn_num.length == 10
 end
+def isbn_length13(isbn_num)
+    isbn_num.length == 13
+end
 def isbn_remove(isbn_num)
-    isbn_num.gsub(/[-]/,"")
+    isbn_num.gsub(/[ ,-]/,"")
 end
