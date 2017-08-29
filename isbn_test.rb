71481e67a8b9dca6b8f4d796ee98e27949b2e0bc
marcfolks <marcfolks@gmail.com>
2017-08-29 15:19:36 -0400

    second_test

--- isbn_test.rb
+++ isbn_test.rb
@@ -2,10 +2,16 @@ require"minitest/autorun"
 require_relative "isbnfunction.rb"
 class Testisbn < Minitest::Test
 
-def test_isbn_number_digits
-    isbn_num="0471958697"
-    assert_equal(true,isbn_length(isbn_num))
+    def test_isbn_number_digits
+        isbn_num="0471958697"
+        assert_equal(true,isbn_length(isbn_num))
+    end
+    def test_isbn_wrong_number_digits
+        isbn_num="047195897"
+        assert_equal(false,isbn_length(isbn_num))
+    end
+    def test_isbn_dashes
+        isbn_num="047-195-86-97"
+        assert_equal("0471958697",isbn_remove(isbn_num))
+    end
 end
-end
-#def test_fist_number_multiplied_by_
-
--- isbnfunction.rb
+++ isbnfunction.rb
@@ -1,3 +1,6 @@
 def isbn_length(isbn_num)
     isbn_num.length == 10
 end
+def isbn_remove(isbn_num)
+    isbn_num.gsub(/[-]/,"")
+end
