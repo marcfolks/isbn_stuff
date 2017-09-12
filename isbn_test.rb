4b4f8cdbf1b68968c47eee43e3284b389bb0dff0
marcfolks <marcfolks@gmail.com>
2017-09-11 23:09:21 -0400

    letterchecking-xconerterwoeks

--- isbn_test.rb
+++ isbn_test.rb
@@ -2,22 +2,26 @@ require"minitest/autorun"
 require_relative "isbnfunction.rb"
 class Testisbn < Minitest::Test
 
-    def test_isbn_number_digits
-        isbn_num="0471958697"
-        assert_equal("valid",isbn_length10(isbn_num))
-    end
-    def test_isbn_wrong_number_digits
-        isbn_num="047195897"
-        assert_equal("invalid",isbn_length10(isbn_num))
-    end
-    def test_isbn_dashes
-        isbn_num="047-195-86-97"
-        assert_equal("0471958697",isbn_remove(isbn_num))
-    end
-     def test_isbn_space
-         isbn_num="047195869j"
-         assert_equal("invalid",alpha_check(isbn_num))
-     end
+    # def test_isbn_number_digits
+    #     isbn_num="0471958697"
+    #     assert_equal("valid",isbn_length10(isbn_num))
+    # end
+    # def test_isbn_wrong_number_digits
+    #     isbn_num="047195897"
+    #     assert_equal("invalid",isbn_length10(isbn_num))
+    # end
+    # def test_isbn_dashes
+    #     isbn_num="047-195-86-97"
+    #     assert_equal("0471958697",isbn_remove(isbn_num))
+    # end
+    #  def test_isbn_space
+    #      isbn_num="047195869J"
+    #      assert_equal("valid",isbncheck(isbn_num))
+    #  end
+       def test_isbn_space2
+         isbn_num="047195869ls"
+         assert_equal("invalid",lettercheck(isbn_num))
+     end 
     # def test_isbn_13
     #     isbn_num="0471958697123"
     #     assert_equal(true,isbn_length13(isbn_num))
@@ -38,7 +42,7 @@ class Testisbn < Minitest::Test
 
     def test_isbn_L
          isbn_num="0471958678"
-         assert_equal("valid",alpha_check(isbn_num))
+         assert_equal("valid",lettercheck(isbn_num))
     end
 #     def test_13isbn_letter
 #         isbn_num="03495812323x"
@@ -47,6 +51,4 @@ class Testisbn < Minitest::Test
 #    # def test_13isbn_character
 #         isbn_num="03495812323@"
 #         assert_equal(false,letter_check(isbn_num))
-#         end
-
-end 
+end
